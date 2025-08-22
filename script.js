aa3817a5d690dd683653972c9a0a77f4acb5bfb1
Dams-O <thefuncraft@gmail.com>
2025-08-22 23:14:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'RGAPI-0527b3db-9051-48b0-8281-68b2c3cef16e'; // Ta clé API valide
+const API_KEY = 'RGAPI-1093a0c5-80ba-43d1-b4f7-3cd1955f7834'; // Ta clé API valide
 const GAME_NAME = 'GLX Dæms'; // Partie avant le #
 const TAG_LINE = 'GLX';
 const REGION = 'europe'; // Région pour l'endpoint account
@@ -47,6 +47,7 @@ function updateProgressBar(points) {
 
 setInterval(fetchMasteryPoints, 3600000); // Mise à jour toutes les heures
 fetchMasteryPoints(); // Appel initial
+
 
 
 
