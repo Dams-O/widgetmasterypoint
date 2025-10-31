3786d4d6ec18639ed8b345b3f470bbd6ae32868a
Dams-O <thefuncraft@gmail.com>
2025-10-31 19:35:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'RGAPI-61aa074f-1373-45f2-841a-4ef5f458ed15'; // Ta clé API valide
+const API_KEY = 'RGAPI-5fe3122c-80a0-4ed0-b8ee-4a8dd6213af2'; // Ta clé API valide
 const GAME_NAME = 'GLX LHomme Saoul'; // Partie avant le #
 const TAG_LINE = 'GLX';
 const REGION = 'europe'; // Région pour l'endpoint account
@@ -47,6 +47,7 @@ function updateProgressBar(points) {
 
 setInterval(fetchMasteryPoints, 3600000); // Mise à jour toutes les heures
 fetchMasteryPoints(); // Appel initial
+
 
 
 
